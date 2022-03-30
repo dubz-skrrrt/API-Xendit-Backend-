7cfad600479d1226e4c013bb02679cc1bfe9fd03
dubz-skrrrt <chill.99.lr@gmail.com>
2022-03-30 16:22:05 +0800

    tokenization id manually input

--- XenditAPI.py
+++ XenditAPI.py
@@ -1,58 +1,47 @@
 import os
 import time
 
+import xendit
 from flask import Flask, request, render_template, jsonify
 from xendit import Xendit, XenditError, BalanceAccountType
-
+import xendit
 app = Flask(__name__)
 payment = [
     {
         'payment1': "CreditCard",
-        # 'credit card number': int(input()),
-        # 'exp date': int(input()),
-        # 'cvn': int (input())
     }
 ]
 @app.route('/')
 def index():
+    print(f"credit-num-{int(time.time())}", )
     return render_template('index.html')
 @app.route('/pay', methods = ['POST'])
-def pay():
+
+def paynow():
+    api_key = "xnd_development_Qlza74hRWMGa4odmMKPAYsH51zryFsnokmHyHEBLrhpuZ4P0J2C3uTCVtgEN"
+    xendit_instance = Xendit(api_key=api_key)
     cardNumber = request.form.get("number")
     expDate = request.form.get("exp_date")
     amount = request.form.get("amount")
+    cvn = request.form.get("CVN")
+
+
     args = {
-        "token_id": "12qwe345rftsdf35",
-        "external_id": f"credit-num-{int(time.time())}",
+        "token_id": '6244121e798b79001c76ac50' ,
+        "external_id": f"card_preAuth-{int(time.time())}",
         "amount": amount,
+        "card_cvn": cvn,
     }
-    api_key = "xnd_development_Qlza74hRWMGa4odmMKPAYsH51zryFsnokmHyHEBLrhpuZ4P0J2C3uTCVtgEN"
-    xendit_instance = Xendit(api_key=api_key)
     try:
         creditPayment = xendit_instance.CreditCard.create_authorization(**args)
+        print(creditPayment)
         return vars(creditPayment)
     except XenditError as e:
+        print(e)
         return vars(e)
-@app.route('/app/api/payment/all')
-def showpayment():
-    return jsonify(payment)
+# @app.route('/app/api/payment/all')
+# def showpayment():
+#     return jsonify(payment)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-#
-#
-# Balance = xendit_instance.Balance
-# CreditCard = xendit_instance.CreditCard
-# balance = Balance.get(
-#     account_type=BalanceAccountType.CASH,
-# )
-# charge = CreditCard.create_authorization(
-#     token_id="5f0410898bcf7a001a00879d",
-#     external_id="card_preAuth-1594106356",
-#     amount=75000,
-#     card_cvn="123",
-# )
-#print(balance)
-#print(charge)
